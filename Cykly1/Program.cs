97c747fbbbebc800541062e2915784b6f4772b9c
Programuj v Truhle <programuj@inina.net>
2010-11-25 02:48:24 +0100

    Prvni commit

---
+++ Cykly1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading; // Tento namespace je třeba pro metodu Thread.Sleep
+
+namespace Cykly
+{
+    class Program
+    {  
+        // Ukázkový program pro zápis iteračních cyklů a základy metod
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Přesně za 2,5 vteřiny napišeš za trest 100 krát : \"Už nikdy nebudu házet křídou po spolužákovi!\" !!");
+            Thread.Sleep(3500); // Uspí hlavní vlákno programu na 2500 milisekund
+
+            // Cha, tak to pro mě není žádný trest...
+            for (int i = 1; i <= 100; i++) // -> provádí se, dokud platí, že i < 100
+                Console.WriteLine("Už nikdy nebudu házet křídou po spolužákovi!");
+
+           
+            
+            Console.WriteLine("Odpočívej 2 vteřiny...");
+            Console.WriteLine("... a pak jistotu ještě jednou to samé 100x...");
+            
+            Thread.Sleep(3000); // Zase uspí program na 3000 milisekund...
+
+            // A teď to samé, jenom zapsáno trochu jinak...
+            int k = 0; 
+            while (k < 100) // -> provádí se, dokud platí, že k < 100
+            {
+                Console.WriteLine("Už nikdy nebudu házet křídou po spolužákovi!");
+                k++;
+            }
+                        
+            
+            Console.WriteLine("Odpočívej 3 vteřiny...");
+            Console.WriteLine("... a pak vypíšeš všechna čísla od 10 do 1...");
+            Thread.Sleep(3000);
+
+            
+            k = (k+1)/10; // přičti ke k jedničku a celé to poděl 10 (=> k = 10)
+
+            for (; k > 0; k--) // -> provádí se, dokud platí k > 0
+               Console.WriteLine("{0}", k);
+
+
+
+            Console.WriteLine(); // Vypíše prázný řádek - tzn. "odřádkuje"
+            Console.WriteLine("Pětkrát pípni!");
+
+            // Cyklus o pěti iteracích zapsaný trošku jinak:
+            for (;;)
+            {
+                if (k == 5) // Když je k rovno pěti ...
+                  break;    //... skončí cyklus a vystoupí z bloku
+
+                Console.WriteLine("\a"); // Tento speciální znak vypsaný do konzole by měl generovat pípnutí speakru
+                ++k;
+            }
+
+
+
+            // ... a nakonec
+            Console.WriteLine("Teď sečteš všechna čísla od 1 do 10...");
+            int součet = SečtiVšechnaČísla(1, 10);
+            Console.WriteLine("Dobře, výsledek je {0}", součet);
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Sčítá všechna čísla v daném rozmezí
+        /// </summary>
+        /// <param name="OdČísla">Dolní mez sumy</param>
+        /// <param name="doČísla">Horní mez sumy</param>
+        /// <returns>Suma všech čísel v daných mezích</returns>
+        static int SečtiVšechnaČísla(int OdČísla, int doČísla)
+        {
+            int výsledek = 0;
+            for (int k = OdČísla; k <= doČísla; k++)
+                výsledek += k;
+
+            return výsledek;
+        }
+    }
+}
---
+++ Cykly2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace Cykly2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Krmivo generátoru vytvoř ze součinu momentální milisekundy a sekundy
+            int mojeKrmivo = DateTime.Now.Millisecond*DateTime.Now.Second;
+            int číslo = mojeKrmivo;
+            
+            // Generuj pseudonáhodné číslo...
+            for (int i = 0; i < DateTime.Now.Second%5; i++)
+               číslo = ZískejPseudonáhodnéČíslo(číslo);            
+
+            // Vypiš číslo...
+            Console.WriteLine("Pseudonáhodné číslo je : {0}, stiskni libovolnou klávesu pro pokračování...", číslo);
+            Console.Read();
+
+            // ...a teď vypiš všechna čísla od 1 do náhodného čísla a přitom přeskakuj všechna sudá
+            VypisujČísla(1, číslo, 2);
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Vypíše všechna čísla vyjma násobků daného čísla
+        /// </summary>
+        /// <param name="Od">Od čísla</param>
+        /// <param name="Do">Do čísla</param>
+        /// <param name="PřeskakujNásobky">Vynechat násobky tohoto čísla</param>
+        static void VypisujČísla(int Od, int Do, int PřeskakujNásobky)
+        {
+            for (int k = Od; k <= Do; k++)
+            {
+                if (k > 0 && k % PřeskakujNásobky == 0)
+                    continue;
+
+                Console.WriteLine("{0}", k);
+            }
+        }
+
+        /// <summary>
+        /// Získá pseudonáhodné číslo von Neumannovou metodou prostředních čtverců
+        /// </summary>
+        /// <param name="Seed">Krmivo generátoru</param>
+        /// <returns>Pseudonáhodné číslo</returns>
+        static int ZískejPseudonáhodnéČíslo(int Seed)
+        {
+            Int64 NaDruhou = Seed * Seed;
+
+            string ČísloJakoŘetězec = NaDruhou.ToString();
+            
+            int délka = ČísloJakoŘetězec.Length;
+
+            while (ČísloJakoŘetězec.Length > (délka / 2)+1)
+            {
+                ČísloJakoŘetězec = ČísloJakoŘetězec.Remove(0, 1);
+                ČísloJakoŘetězec = ČísloJakoŘetězec.Remove(ČísloJakoŘetězec.Length - 1, 1);
+            }
+
+            return int.Parse(ČísloJakoŘetězec);
+            
+        }
+    }
+}
